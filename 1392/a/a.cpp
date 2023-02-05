a0fb85837ebc5e4ba7cb7a47f7ab89a9f82727eb
manncodes <manncodes@gmail.com>
2023-02-05 17:56:32 +0530

    '✨1392'

---
+++ 1392/a/a.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+    cout << (*min_element(a.begin(), a.end()) == *max_element(a.begin(), a.end()) ? n : 1) << '\n';
+  }
+  return 0;
+}
