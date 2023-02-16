25bebfe0f5cc85a25cbfefebe00e3060ea2ea29d
manncodes <manncodes@gmail.com>
2023-02-16 14:10:32 +0530

    '🎡263'

---
+++ 263/a/a.cpp
@@ -0,0 +1,70 @@
+/*
+    Author: manncodes
+    Time : 07 January 2023  11:58:01
+    File: a.cpp
+    bc1064f6-88ad-41a7-b2a2-fa840f172c87
+"*/
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace std;
+
+using namespace __gnu_pbds;
+template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k) -> iterator of kth element (in sorted fashion : low - high)
+// order_of_key(x) -> #numbers strictly less than x
+
+#pragma GCC optimize("Ofast","unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC target("avx2")
+
+#define sync ios_base::sync_with_stdio(0); cin.tie(0);
+#define all(x) x.begin(),x.end()
+#define unq(a) sort(all(a));a.resize(unique(all(a)) - a.begin())
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--) 
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x).size()
+
+#define endl '\n'
+
+using pii = pair<int, int>;
+using ll = long long;
+using ld = long double;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//mt19937 rng(0);
+
+void solve() {
+    int N = 5;
+    int x = 0, y = 0;
+    int xx = 2, yy = 2;
+    vector<vector<int>> a(N, vector<int>(N));
+    rep(i, 0, N) {
+        rep(j, 0, N) {
+            cin >> a[i][j];
+            if(a[i][j] == 1) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+    cout << abs(x - xx) + abs(y - yy) << endl;
+}
+
+int main() {
+    //    #ifndef ONLINE_JUDGE 
+    //       freopen("input.txt", "r", stdin);
+    //       freopen("output.txt", "w", stdout);
+    //    #endif
+
+    sync
+
+        int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    cerr << "processor time: " << clock() / (double)CLOCKS_PER_SEC << "s    ";
+    return 0;
+}
